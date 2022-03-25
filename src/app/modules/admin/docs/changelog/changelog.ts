b94c0d363b231a54b4100d2a9f84b5ec77193d65
Ahmad Miqdaad <ahmad.miqdaad@kalsym.com>
2022-03-25 11:51:26 +0800

    add changelog

---
+++ src/app/modules/admin/docs/changelog/changelog.ts
@@ -0,0 +1,61 @@
+/* eslint-disable max-len */
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+    selector       : 'changelog',
+    templateUrl    : './changelog.html',
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ChangelogComponent
+{
+    changelog: any[] = [
+        // v0.0.1
+        {
+            version    : 'v0.0.1',
+            releaseDate: 'Mar 25, 2022',
+            changes    : [
+                {
+                    type: 'Added',
+                    list: [
+                        '(Changelog) Added login, signup, reset password, logout page',
+                        '(Changelog) Added the ChangeLog page'
+                    ]
+                }
+            ]
+        },
+        // v13.6.0
+        // {
+        //     version    : 'v13.6.0',
+        //     releaseDate: 'Aug 31, 2021',
+        //     changes    : [
+        //         {
+        //             type: 'Added',
+        //             list: [
+        //                 '(QuickChat) Added the QuickChat bar'
+        //             ]
+        //         },
+        //         {
+        //             type: 'Changed',
+        //             list: [
+        //                 '(dependencies) Updated Angular & Angular Material to v12.2.3',
+        //                 '(dependencies) Updated various other packages',
+        //                 '(layout) Separated the Settings drawer from the layout component'
+        //             ]
+        //         },
+        //         {
+        //             type: 'Fixed',
+        //             list: [
+        //                 '(@fuse/drawer) Final opacity of the overlay is not permanent due to player being destroyed right after the animation'
+        //             ]
+        //         }
+        //     ]
+        // }
+    ];
+
+    /**
+     * Constructor
+     */
+    constructor()
+    {
+    }
+}
