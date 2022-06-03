63b94c234bf02b10e63b36075eccda079420f1d2
Wafi <ahmad.wafi@kalsym.com>
2022-06-03 13:01:58 +0800

    Store fixes

--- src/app/modules/landing/stores/stores.resolvers.ts
+++ src/app/modules/landing/stores/stores.resolvers.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRoute, ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from '@angular/router';
 import { forkJoin, Observable, of, throwError } from 'rxjs';
 import { catchError, switchMap, take } from 'rxjs/operators';
 import { StoreCategory, Store, StorePagination } from 'app/core/store/store.types';
 import { StoresService } from 'app/core/store/store.service';
 import { LocationService } from 'app/core/location/location.service';
 import { PlatformService } from 'app/core/platform/platform.service';
+import { map } from 'lodash';
 
 @Injectable({
     providedIn: 'root'
 })
 export class StoresResolver implements Resolve<any>
 {
+    storeDomain: string;
     /**
      * Constructor
      */
@@ -20,6 +22,7 @@ export class StoresResolver implements Resolve<any>
         private _storesService: StoresService,
         private _locationService: LocationService,
         private _platformsService: PlatformService,
+        private _route: ActivatedRoute,
 
     )
     {
@@ -35,48 +38,18 @@ export class StoresResolver implements Resolve<any>
     * @param route
     * @param state
     */
-    // resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):  Observable<any>
-    // {
-    //     console.log('StoresResolver');
-        
-    //     return forkJoin([
-    //         this._storesService.getStores("",0,10,"MYS","created","desc"),
-    //         this._locationService.getFeaturedStores(0, 20, response.platformCountry,"created","desc").subscribe(()=>{});
-    //     ]);
-        
-    // }
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any>
     {
 
-        // this._platformsService.platform$
-        // .pipe(takeUntil(this._unsubscribeAll))
-        // .subscribe((platform: Platform) => { 
+        this.storeDomain = this._route.snapshot.paramMap.get('store-slug')   
 
-        //     this.platform = platform;  
+        return this._storesService.getStoreByDomainName(route.paramMap.get('store-slug'))
+            .pipe(
+                switchMap((store) => {
 
-        //     this._locationService.getFeaturedStores(0, this.mobileView ? 5 : 10, platform.country)
-        //     .subscribe((stores) => {
-        //         console.log(stores);
-                
-        //         this.featuredStores = stores;  
-                
-        //     })  
-    
-        //     this._changeDetectorRef.markForCheck();
+                    return this._storesService.getStoreCategories(store.id)
+                })
+            );
 
-        // });
-
-        return forkJoin([
-            // this._platformsService.platform$
-            // .pipe(
-            //     take(1),
-            //     switchMap((response) => {                            
-            //         // this._storesService.getStores("",0,10,response.platformCountry,"created","desc").subscribe(()=>{});
-            //         this._locationService.getFeaturedStores({pageSize:20, regionCountryId:response.country}).subscribe(()=>{});
-            //         return of(true);
-            //     })
-            // ),
-            // this._locationService.getFeaturedStores(0, 20, response.platformCountry,"created","desc").subscribe(()=>{});
-        ]);
     }
 }
